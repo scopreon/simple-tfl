63b02bea31fcfe787d0993f2d28ee568a41afaf0
Saul Cooperman <sc21sc@leeds.ac.uk>
2025-11-16 02:11:29 +0000

    Make thread safe

--- src/server/cache.py
+++ src/server/cache.py
@@ -3,6 +3,7 @@ import time
 from collections.abc import Callable
 from dataclasses import dataclass
 from functools import wraps
+from threading import Lock
 from typing import ParamSpec, TypeVar
 
 
@@ -19,25 +20,33 @@ class CacheEntry[T]:
 # this is NOT thread safe
 def cache_with_ttl(*, ttl: float) -> Callable[[Callable[P, T]], Callable[P, T]]:
     def decorator(f: Callable[P, T]) -> Callable[P, T]:
+        mutex_lock = Lock()
+
         _results: dict[tuple, CacheEntry[T]] = {}
         sig = inspect.signature(f)
 
         @wraps(f)
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
-            current_epoch = time.time()
+            current_epoch = time.monotonic()
 
             bound = sig.bind(*args, **kwargs)
             bound.apply_defaults()
 
             hashed = tuple((name, bound.arguments[name]) for name in sig.parameters)
 
-            if hashed in _results:
-                if _results[hashed].creation_time + ttl > current_epoch:
-                    return _results[hashed].result
-                _results.pop(hashed)
+            with mutex_lock:
+                entry = _results.get(hashed)
+                if entry and entry.creation_time + ttl > current_epoch:
+                    return entry.result
 
             ret = f(*args, **kwargs)
-            _results[hashed] = CacheEntry(creation_time=current_epoch, result=ret)
+
+            with mutex_lock:
+                entry = _results.get(hashed)
+                if entry and entry.creation_time + ttl > current_epoch:
+                    return entry.result
+                _results[hashed] = CacheEntry(creation_time=current_epoch, result=ret)
+
             return ret
 
         return wrapper
