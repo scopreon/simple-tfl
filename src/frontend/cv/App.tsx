503ccf49a690e69cb56469636c31206aed3c7716
Saul Cooperman <sc21sc@leeds.ac.uk>
2026-01-19 00:39:35 +0000

    Fix path

--- src/frontend/cv/App.tsx
+++ src/frontend/cv/App.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
     <iframe
-      src="/cv/Saul_Cooperman_CV.pdf"
+      src={`${import.meta.env.BASE_URL}cv/Saul_Cooperman_CV.pdf`}
       style={{ flex: 1, width: '100%', border: 'none' }}
       title="CV"
     />
