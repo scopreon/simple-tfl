2455c9718e5ede8bd23c1893186a769ab84ba355
Saul Cooperman <sc21sc@leeds.ac.uk>
2026-01-19 21:26:00 +0000

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-const backendPort = Number(process.env.BACKEND_PORT || 8000);
-
 // https://vite.dev/config/
 export default defineConfig(() => ({
   root: path.resolve(__dirname, 'src/frontend/compile'),
   publicDir: path.resolve(__dirname, 'src/frontend/static'),
   plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, 'dist/frontend'),
+    outDir: path.resolve(__dirname, 'src/frontend/dist'),
     emptyOutDir: true,
   },
 }));
